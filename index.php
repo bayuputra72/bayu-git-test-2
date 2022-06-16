6d82d1ecf1ff669a2c96daced28d5610f6204478
bayuputra_72 <bayusingaraja27@gmail.com>
2022-06-16 13:14:04 +0800

    Menambahkan paragraf pada file index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ea dicta assumenda alias odio explicabo autem beatae quae! Pariatur voluptas natus quo, praesentium molestiae explicabo numquam repellendus aperiam quos tempore?</p>
 </body>
 
 </html>
